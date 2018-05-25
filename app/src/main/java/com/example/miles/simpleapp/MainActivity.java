73e963ac06c0a559c1128749faaef0fb2b872bf4
cpham30 <christopherpham108@gmail.com>
2018-05-25 01:14:08 -0400

    Added data retrieval for most recent value and implemented getValue button.

--- app/src/main/java/com/example/miles/simpleapp/MainActivity.java
+++ app/src/main/java/com/example/miles/simpleapp/MainActivity.java
@@ -2,19 +2,24 @@ package com.example.miles.simpleapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button storeButton, getButton;
-    private EditText storeValue;
+    private EditText storeValue, getValue;
     private FirebaseDatabase database;
     private DatabaseReference myRef;
+    private String currentValue; //most recently stored value
 
 
     @Override
@@ -25,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
         storeButton = (Button) findViewById(R.id.storeButton);
         getButton = (Button) findViewById(R.id.getButton);
         storeValue = (EditText) findViewById(R.id.storeValue);
+        getValue = (EditText) findViewById(R.id.getValue);
 
         database = FirebaseDatabase.getInstance();
         myRef = database.getReference().child("values");
@@ -39,11 +45,28 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and
+                // every time data at this location is updated.
+                currentValue = (String) Long.toString((Long) dataSnapshot.getValue());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Failed to read value
+                System.out.println("The read failed: " + databaseError.getMessage());
+            }
+        });
+
         getButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //TODO: retrieve most recently stored value in Firebase
-
+                getValue.setText(currentValue);
             }
         });
     }
